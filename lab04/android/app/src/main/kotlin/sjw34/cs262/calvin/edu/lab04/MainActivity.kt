c9d621285095992b4334813a0652632b2bbe049f
Sameer Mall <sameerm@pectechnologies.com>
2019-09-26 19:21:25 -0400

    Lab 04 Upload

---
+++ lab04/android/app/src/main/kotlin/sjw34/cs262/calvin/edu/lab04/MainActivity.kt
@@ -0,0 +1,13 @@
+package sjw34.cs262.calvin.edu.lab04
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
