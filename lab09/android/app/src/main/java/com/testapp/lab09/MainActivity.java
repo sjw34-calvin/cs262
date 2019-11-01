e89bc38e02f3cbddbb13d700ba7c68f9439c9aab
Sameer Mall <sameerm@pectechnologies.com>
2019-11-01 19:14:34 -0400

    Lab 09 upload

---
+++ lab09/android/app/src/main/java/com/testapp/lab09/MainActivity.java
@@ -0,0 +1,13 @@
+package com.testapp.lab09;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
